2b603c53e5e286dd88495daa15f3c1021b92e53a
zhang_ji <zhang_ji@worksap.co.jp>
2016-06-13 13:56:42 +0800

    [main] reader/matcher communication done.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate libc;
 extern crate ncurses;
 
+mod util;
+
 use std::io::{stdin, Read, BufRead, BufReader};
 use std::error::Error;
 use std::sync::{Arc, Mutex};
@@ -8,6 +10,8 @@ use std::thread;
 use std::process::{Command, Stdio, exit};
 use std::char;
 use std::mem;
+use std::sync::mpsc::{Sender, Receiver, channel};
+use util::eventbox::EventBox;
 
 use ncurses::*;
 
@@ -18,44 +22,86 @@ struct FZF {
 }
 
 //==============================================================================
-enum MatcherEvent{
-    QUERY_CHANG,
-    SELECT,
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+enum Event{
+    EV_READER_NEW,
+    EV_READER_FIN,
+    EV_MATCHER_NEW_ITEM,
+    EV_MATCHER_RESET_QUERY,
+    EV_MATCHER_UPDATE_PROCESS,
+    EV_MATCHER_FINISHED,
 }
+
+// matcher will receive two events:
+// 1. EV_MATCHER_NEW_ITEM, to reset the input strings
+// 2. EV_MATCHER_RESET_QUERY, to interrupt current processing.
+//
+// will send two events:
+// 1. EV_MATCHER_UPDATE_PROCESS, to notify the matched/total items
+// 2. EV_MATCHER_FINISHED.
 
 struct Matcher {
+    rx_source: Receiver<String>, // channel to retrieve strings from reader
+    tx_output: Sender<String>,   // channel to send output to
+    eb_req: Arc<EventBox<Event>>,       // event box that recieve requests
+    eb_notify: Arc<EventBox<Event>>,    // event box that send out notification
+    items: Vec<String>,
+    item_pos: usize,
     query: String,
-    mutex_items: Arc<Mutex<Vec<String>>>,
-    matches: Vec<String>,
-    display: Display,
 }
+    
 
 impl Matcher {
-    pub fn new(mtx_items: Arc<Mutex<Vec<String>>>) -> Self {
-        Matcher {
-            query: "my query".to_owned(),
-            mutex_items: mtx_items,
-            matches: vec!["one".to_owned(), "two".to_owned()],
-            display: Display::new(),
+    pub fn new(rx_source: Receiver<String>, tx_output: Sender<String>,
+               eb_req: Arc<EventBox<Event>>, eb_notify: Arc<EventBox<Event>>) -> Self {
+        Matcher{
+            rx_source: rx_source,
+            tx_output: tx_output,
+            eb_req: eb_req,
+            eb_notify: eb_notify,
+            items: Vec::new(),
+            item_pos: 0,
+            query: String::new(),
         }
     }
 
-    pub fn display_items(&mut self) {
-        let mut items = self.mutex_items.lock().unwrap();
-        let maxc = self.display.max_x - 3;
-        self.display.prepare_print_line();
-        for item in (*items).iter() {
-            self.display.print_line(item);
+    pub fn process(&mut self) {
+        for string in self.items[self.item_pos..].into_iter() {
+            // process the matcher
+            //self.tx_output.send(string.clone());
+            (*self.eb_notify).set(Event::EV_MATCHER_UPDATE_PROCESS, Box::new(0));
+            self.tx_output.send(string.clone());
+
+            self.item_pos += 1;
+            if (self.item_pos % 100) == 99 && !self.eb_req.is_empty() {
+                break;
+            }
         }
     }
 
-    pub fn display_info(&self) {
-        // XX/YY file(s)
-        self.display.print_info(format!("  {}/{} file(s)", 10, 20).as_ref());
+    fn read_new_item(&mut self) {
+        while let Ok(string) = self.rx_source.try_recv() {
+            self.items.push(string);
+        }
+    }
+
+    fn reset_query(&mut self, query: &str) {
+        self.query.clear();
+        self.query.push_str(query);
     }
 
     pub fn run(&mut self) {
+        loop {
+            for (e, val) in (*self.eb_req).wait() {
+                match e {
+                    Event::EV_MATCHER_NEW_ITEM => { self.read_new_item();}
+                    Event::EV_MATCHER_RESET_QUERY => {self.reset_query(*val.downcast().unwrap());}
+                    _ => {}
+                }
+            }
 
+            self.process()
+        }
     }
 }
 
@@ -176,33 +222,30 @@ impl Queryer {
 
 const READER_LINES_CACHED: usize = 100;
 
-// invoke find comand.
-fn get_command_output() -> Result<Box<BufRead>, Box<Error>> {
-    let command = try!(Command::new("ls")
-                       .arg(".")
-                       .stdout(Stdio::piped())
-                       .stderr(Stdio::null())
-                       .spawn());
-    let stdout = try!(command.stdout.ok_or("command output: unwrap failed".to_owned()));
-    Ok(Box::new(BufReader::new(stdout)))
+struct Reader {
+    cmd: Option<&'static str>, // command to invoke
+    eb: Arc<EventBox<Event>>,         // eventbox
+    tx: Sender<String>,    // sender to send the string read from command output
 }
 
-struct Reader {
-    lines: Vec<String>,
-    mutex_output: Arc<Mutex<Vec<String>>>,
-}
+    
 
 impl Reader {
-    pub fn new(mtx: Arc<Mutex<Vec<String>>>) -> Self {
-        Reader {
-            lines: vec![],
-            mutex_output: mtx,
-        }
+
+    pub fn new(cmd: Option<&'static str>, eb: Arc<EventBox<Event>>, tx: Sender<String>) -> Self {
+        Reader{cmd: cmd, eb: eb, tx: tx}
     }
 
-    fn update_output(&mut self) {
-        let mut items = self.mutex_output.lock().unwrap();
-        (*items).extend(mem::replace(&mut self.lines, vec![]));
+    // invoke find comand.
+    fn get_command_output(&self) -> Result<Box<BufRead>, Box<Error>> {
+        let command = try!(Command::new("sh")
+                           .arg("-c")
+                           .arg(self.cmd.unwrap_or("find ."))
+                           .stdout(Stdio::piped())
+                           .stderr(Stdio::null())
+                           .spawn());
+        let stdout = try!(command.stdout.ok_or("command output: unwrap failed".to_owned()));
+        Ok(Box::new(BufReader::new(stdout)))
     }
 
     fn run(&mut self) {
@@ -211,7 +254,7 @@ impl Reader {
 
         let mut read;
         if istty {
-            read = get_command_output().expect("command not found: find");
+            read = self.get_command_output().expect("command not found");
         } else {
             read = Box::new(BufReader::new(stdin()))
         };
@@ -228,17 +271,13 @@ impl Reader {
                             input.pop();
                         }
                     }
-
-                    self.lines.push(input);
-                    if self.lines.len() >= READER_LINES_CACHED {
-                        self.update_output();
-                    }
-
+                    self.tx.send(input);
                 }
                 Err(_err) => { break; }
             }
+            self.eb.set(Event::EV_READER_NEW, Box::new(0));
         }
-        self.update_output();
+        self.eb.set(Event::EV_READER_FIN, Box::new(0));
     }
 }
 
@@ -253,20 +292,60 @@ fn main() {
     keypad(stdscr, true);
     noecho();
 
-    // initialize ncurses screen
-    let mtx_item = Arc::new(Mutex::new(vec![]));
 
-    // reader
-    let reader_mtx_item = mtx_item.clone();
-    let mut reader = Reader::new(reader_mtx_item);
-    let th_reader = thread::spawn(move || reader.run());
+    let eb = Arc::new(EventBox::new());
+    let eb_clone_reader = eb.clone();
+    let eb_clone_matcher = eb.clone();
+    let (tx_source, rx_source) = channel();
+    let (tx_matched, rx_matched) = channel();
+    let mut reader = Reader::new(Some(&"find ."), eb_clone_reader, tx_source);
+    let eb_matcher = Arc::new(EventBox::new());
+    let eb_matcher_clone = eb_matcher.clone();
+    let mut matcher = Matcher::new(rx_source, tx_matched, eb_matcher_clone, eb_clone_matcher);
 
-    let mut matcher = Matcher::new(mtx_item);
+    // start running
+    thread::spawn(move || {
+        reader.run();
+    });
+
+    thread::spawn(move || {
+        matcher.run();
+    });
+
+    let mut items = vec![];
+
     loop {
-        matcher.display_items();
-        matcher.display.refresh();
-        getch();
-    }
+        for (e, val) in eb.wait() {
+            match e {
+                Event::EV_READER_NEW => {
+                    //printw("READER_NEW!\n");
+                    eb_matcher.set(Event::EV_MATCHER_NEW_ITEM, Box::new(0));
+                }
+                Event::EV_READER_FIN => {
+                    //printw("READER_FIN\n");
+                }
+                Event::EV_MATCHER_UPDATE_PROCESS => {
+                    printw(format!("UPDATE_PROCESS: {}\n", items.len()).as_str());
+                    while let Ok(string) = rx_matched.try_recv() {
+                        items.push(string);
+                    }
+                }
+                _ => {
+                    printw(format!("{}\n", e as i32).as_str());
+                }
+            }
+        }
+
+        // print items
+        let mut y = 0;
+        for string in items.iter() {
+            mv(y, 0);
+            printw(string);
+            y += 1;
+        }
+
+        refresh();
+    };
 
     //let disp = Display::new();
     //disp.init();
@@ -298,6 +377,6 @@ fn main() {
 
     //getch();
     //endwin();
-    th_reader.join();
+    //th_reader.join();
     //displayer.join();
 }
