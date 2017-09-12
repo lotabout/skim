9bbe12749d7ec6f4afee932a1f7d027da999710b
zhang_ji <lotabout@gmail.com>
2017-09-12 16:15:24 +0800

    remove ncurses dependency, only termion is needed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 extern crate libc;
-extern crate ncurses;
 extern crate getopts;
 extern crate regex;
 extern crate shlex;
