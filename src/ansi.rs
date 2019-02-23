ebb23f2f48b6ba1a3ea605b5dee66241543c1691
Jinzhou Zhang <lotabout@gmail.com>
2019-02-23 17:48:30 +0800

    [ansi] use vte to parse ANSI codes

--- src/ansi.rs
+++ src/ansi.rs
@@ -1,68 +1,201 @@
 // Parse ANSI attr code
-use crate::curses::{attr_t, register_ansi, Window};
-use regex::Regex;
+use tuikit::attr::{Attr, Effect, Color};
 use std::default::Default;
-use std::iter::Enumerate;
-use std::iter::Peekable;
+use vte::Perform;
+use std::mem;
 
+/// An ANSI Parser, will parse one line at a time.
+///
+/// It will cache the latest attribute used, that means if an attribute affect multiple
+/// lines, the parser will recognize it.
 pub struct ANSIParser {
-    re: &'static Regex,
-    last_attr: Option<attr_t>,
-}
-
-lazy_static! {
-    static ref ANSI_RE: Regex =
-        Regex::new(r"\x1B\[(?:([0-9]+;[0-9]+[Hf])|([0-9]+[ABCD])|(s|u|2J|K)|([0-9;]*m)|(=[0-9]+[hI]))").unwrap();
+    partial_str: String,
+    fragments: Vec<(Attr, String)>,
+    last_attr: Attr,
 }
 
 impl Default for ANSIParser {
     fn default() -> Self {
         ANSIParser {
-            re: &ANSI_RE,
-            last_attr: None,
+            partial_str: String::new(),
+            fragments: Vec::new(),
+            last_attr: Attr::default(),
         }
     }
 }
 
+impl Perform for ANSIParser {
+    fn print(&mut self, ch: char) {
+        self.partial_str.push(ch);
+    }
+
+    fn execute(&mut self, byte: u8) {
+        match byte {
+            // put back \n \t
+            0x0A | 0x09 => self.partial_str.push(byte as char),
+            // ignore all others
+            _ => trace!("AnsiParser:execute ignored {:?}", byte),
+        }
+    }
+
+    fn hook(&mut self, params: &[i64], _intermediates: &[u8], _ignore: bool) {
+        trace!("AnsiParser:hook ignored {:?}", params);
+    }
+
+    fn put(&mut self, byte: u8) {
+        trace!("AnsiParser:put ignored {:?}", byte);
+    }
+
+    fn unhook(&mut self) {
+        trace!("AnsiParser:unhook ignored");
+    }
+
+    fn osc_dispatch(&mut self, params: &[&[u8]]) {
+        trace!("AnsiParser:osc ignored {:?}", params);
+    }
+
+    fn csi_dispatch(&mut self, params: &[i64], _intermediates: &[u8], _ignore: bool, mode: char) {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
+        // Only care about graphic modes, ignore all others
+
+        if mode != 'm' {
+            trace!("ignore: params: {:?}, mode: {:?}", params, mode);
+            return;
+        }
+
+        let mut attr = self.last_attr;
+        match params.len() {
+            // CSI m, reset
+            0 => attr = Attr::default(),
+            // CSI <num> m
+            1 => match params[0] {
+                0 => attr = Attr::default(),
+                1 => attr.effect |= Effect::BOLD,
+                4 => attr.effect |= Effect::UNDERLINE,
+                5 => attr.effect |= Effect::BLINK,
+                7 => attr.effect |= Effect::REVERSE,
+                num if num >= 30 && num <= 37 => {
+                    attr.fg = Color::AnsiValue((num - 30) as u8);
+                }
+                num if num >= 40 && num <= 47 => {
+                    attr.bg = Color::AnsiValue((num - 40) as u8);
+                }
+                39 => attr.fg = Color::Default,
+                49 => attr.bg = Color::Default,
+                _ => {
+                    trace!("ignore CSI {:?} m", params);
+                }
+            },
+            // ESC[ 38;5;<n> m Select foreground color
+            // ESC[ 48;5;<n> m Select background color
+            3 => {
+                if params[1] != 5 {
+                    trace!("ignore CSI {:?} m", params);
+                } else {
+                    let color = Color::AnsiValue(params[2] as u8);
+                    if params[0] == 38 {
+                        attr.fg = color;
+                    } else if params[0] == 48 {
+                        attr.bg = color;
+                    }
+                }
+            }
+            // ESC[ 38;2;<r>;<g>;<b> m Select RGB foreground color
+            // ESC[ 48;2;<r>;<g>;<b> m Select RGB background color
+            5 => {
+                if params[1] != 2 {
+                    trace!("ignore CSI {:?} m", params);
+                } else {
+                    let r = params[2] as u8;
+                    let g = params[3] as u8;
+                    let b = params[4] as u8;
+                    let color = Color::Rgb(r, g, b);
+                    if params[0] == 38 {
+                        attr.fg = color;
+                    } else if params[0] == 48 {
+                        attr.bg = color;
+                    }
+                }
+            }
+            _ => trace!("ignore CSI {:?} m", params),
+        }
+
+        self.attr_change(attr);
+    }
+
+    fn esc_dispatch(&mut self, _params: &[i64], _intermediates: &[u8], _ignore: bool, _byte: u8) {
+        // ESC characters are replaced with \[
+        self.partial_str.push('"');
+        self.partial_str.push('[');
+    }
+}
+
+impl ANSIParser {
+    /// save the partial_str into fragments with current attr
+    fn save_str(&mut self) {
+        if self.partial_str.is_empty() {
+            return;
+        }
+
+        let str = mem::replace(&mut self.partial_str, String::new());
+        self.fragments.push((self.last_attr, str));
+    }
+
+    // accept a new attr
+    fn attr_change(&mut self, new_attr: Attr) {
+        if new_attr == self.last_attr {
+            return;
+        }
+
+        self.save_str();
+        self.last_attr = new_attr;
+    }
+
+    pub fn parse_ansi(&mut self, text: &str) -> AnsiString {
+        let mut statemachine = vte::Parser::new();
+
+        for byte in text.as_bytes() {
+            statemachine.advance(self, *byte);
+        }
+        self.save_str();
+
+        let fragments = mem::replace(&mut self.fragments, Vec::new());
+        AnsiString {
+            fragments,
+        }
+    }
+}
+
+
 #[derive(Clone, Debug)]
-// named like this not clash with ANSIString from ansi_term crate
+/// A String that contains ANSI state (e.g. colors)
+///
+/// It is internally represented as Vec<(attr, string)>
 pub struct AnsiString {
-    pub stripped: String,
-    pub ansi_states: Vec<(usize, attr_t)>,
+    fragments: Vec<(Attr, String)>,
 }
 
 impl AnsiString {
-
     pub fn new_empty() -> AnsiString {
         AnsiString {
-            stripped: "".to_string(),
-            ansi_states: Vec::new(),
+            fragments: Vec::new(),
         }
     }
 
     pub fn into_inner(self) -> String {
-        self.stripped
-    }
-
-    pub fn print(&self, curses: &mut Window) {
-        for (c, attrs) in self.iter() {
-            for (_, a) in attrs {
-                curses.attr_on(*a)
-            }
-            curses.addch(c);
+        let mut ret = String::new();
+        for (_attr, string) in self.fragments.into_iter() {
+            ret.push_str(&string);
         }
+        ret
     }
 
     pub fn iter(&self) -> AnsiStringIterator {
-        AnsiStringIterator {
-            ansi_states: &self.ansi_states,
-            it_text: Box::new(self.stripped.chars().enumerate()),
-            pk_ansi_states: self.ansi_states.iter().enumerate().peekable(),
-        }
+        AnsiStringIterator::new(&self.fragments)
     }
 
     pub fn has_attrs(&self) -> bool {
-        !self.ansi_states.is_empty()
+        self.fragments.len() > 1
     }
 
     pub fn from_str(raw: &str) -> AnsiString {
@@ -70,162 +203,42 @@ impl AnsiString {
     }
 }
 
+/// An iterator over all the (char, attr) characters.
 pub struct AnsiStringIterator<'a> {
-    ansi_states: &'a Vec<(usize, attr_t)>,
-    it_text: Box<Enumerate<std::str::Chars<'a>>>,
-    pk_ansi_states: Peekable<Enumerate<std::slice::Iter<'a, (usize, attr_t)>>>,
+    fragments: &'a Vec<(Attr, String)>,
+    fragment_idx: usize,
+    attr: Attr,
+    chars_iter: Option<std::str::Chars<'a>>,
+}
+
+impl<'a> AnsiStringIterator<'a> {
+    pub fn new(fragments: &'a Vec<(Attr, String)>) -> Self {
+        Self {
+            fragments,
+            fragment_idx: 0,
+            attr: Attr::default(),
+            chars_iter: None,
+        }
+    }
 }
 
 impl<'a> Iterator for AnsiStringIterator<'a> {
-    type Item = (char, &'a [(usize, attr_t)]);
+    type Item = (char, Attr);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut as_range: Option<(usize, usize)> = None;
-        let (ch_idx, ch) = match self.it_text.next() {
-            Some((ch_idx, ch)) => (ch_idx, ch),
-            None => {
-                return None;
-            }
-        };
-
-        while let Some(&(states_idx, &(ansi_idx, _))) = self.pk_ansi_states.peek() {
-            if ch_idx == ansi_idx {
-                let _ = self.pk_ansi_states.next();
-                as_range = match as_range {
-                    Some((start, end)) => Some((start, end + 1)),
-                    None => Some((states_idx, states_idx)),
-                }
-            } else if ch_idx > ansi_idx {
-                let _ = self.pk_ansi_states.next();
-            } else {
-                break;
-            }
-        }
-        if let Some((start, end)) = as_range {
-            return Some((ch, &self.ansi_states[start..=end]));
-        } else {
-            return Some((ch, &[]));
-        }
-    }
-}
-
-impl ANSIParser {
-    pub fn parse_ansi(&mut self, text: &str) -> AnsiString {
-        let mut strip_string = String::new();
-        let mut colors = Vec::new();
-
-        // assume parse_ansi is called linewise.
-        // Because ANSI color code can affect text of next lines. We will save the last attribute and
-        // add it to the newest line if no new color is specified.
-        match self.re.find(text) {
-            Some(mat) if mat.start() == 0 => {}
-            _ => {
-                if let Some(attr) = self.last_attr {
-                    colors.push((0, attr));
-                }
-            }
-        }
-
-        let mut num_chars = 0;
-        let mut last = 0;
-        for mat in self.re.find_iter(text) {
-            let (start, end) = (mat.start(), mat.end());
-            strip_string.push_str(&text[last..start]);
-            num_chars += (&text[last..start]).chars().count();
-
-            last = end;
-
-            let attr = self.interpret_code(&text[start..end]);
-            if let Some(attr) = attr {
-                colors.push((num_chars, attr));
-            }
-            self.last_attr = attr;
-        }
-
-        strip_string.push_str(&text[last..text.len()]);
-
-        AnsiString {
-            stripped: strip_string,
-            ansi_states: colors,
-        }
-    }
-
-    fn interpret_code(&self, code: &str) -> Option<attr_t> {
-        if code == "\x1B[K" || code == "\x1B[2J" {
-            // clear screen & clear line
+        let ch = self.chars_iter.as_mut().and_then(|iter| iter.next());
+        if ch.is_some() {
+            Some((ch.unwrap(), self.attr))
+        } else if ch.is_none() && self.fragment_idx >= self.fragments.len() {
             None
         } else {
-            let key = register_ansi(code.to_owned());
-            Some(key)
+            // try next fragment
+            let (attr, string) = &self.fragments[self.fragment_idx];
+            self.attr = *attr;
+            self.chars_iter.replace(string.chars());
+            self.fragment_idx += 1;
+            self.next()
         }
-
-        //let mut state256 = 0;
-        //let mut attr = 0;
-        //let mut fg = -1;
-        //let mut bg = -1;
-        //let mut use_fg = true;
-
-        //let code = &code[2..code.len()-1]; // ^[[1;30;40m -> 1;30;40
-        //if code.is_empty() {
-        //return Some(A_NORMAL());
-        //}
-
-        //for num in code.split(';').map(|x| x.parse::<i16>()) {
-        //match state256 {
-        //0 => {
-        //match num.unwrap_or(0) {
-        //0 => {attr = 0;}
-        //1 => {attr |= A_BOLD();}
-        //4 => {attr |= A_UNDERLINE();}
-        //5 => {attr |= A_BLINK();}
-        //7 => {attr |= A_REVERSE();}
-        //8 => {attr |= A_INVIS();}
-        //38 => {
-        //use_fg = true;
-        //state256 += 1;
-        //}
-        //48 => {
-        //use_fg = false;
-        //state256 += 1;
-        //}
-        //39 => {
-        //fg = -1;
-        //}
-        //49 => {
-        //bg = -1;
-        //}
-        //num if num >= 30 && num <= 37 => {
-        //fg = num - 30;
-        //}
-        //num if num >= 40 && num <= 47 => {
-        //bg = num - 40;
-        //}
-        //_ => {
-        //}
-        //}
-        //}
-        //1 => {
-        //match num.unwrap_or(0) {
-        //5 => { state256 += 1; }
-        //_ => { state256 = 0; }
-        //}
-        //}
-        //2 => {
-        //if use_fg {
-        //fg = num.unwrap_or(-1);
-        //} else {
-        //bg = num.unwrap_or(-1);
-        //}
-        //}
-        //_ => {}
-        //}
-        //}
-
-        //if fg != -1 || bg != -1 {
-        //attr |= get_color_pair(fg, bg);
-        //}
-
-        //Some(attr)
     }
 }
 
@@ -238,8 +251,29 @@ mod tests {
         let input = "\x1B[48;2;5;10;15m\x1B[38;2;70;130;180mhi\x1B[0m";
         let ansistring = ANSIParser::default().parse_ansi(input);
         let mut it = ansistring.iter();
-        let arr: Vec<(usize, u16)> = vec![(0, 11), (0, 12)];
-        assert_eq!(Some(('h', &arr[..2])), it.next());
-        assert_eq!(Some(('i', &arr[..0])), it.next());
+        let attr = Attr {
+            fg: Color::Rgb(70, 130, 180),
+            bg: Color::Rgb(5, 10, 15),
+            ..Attr::default()};
+
+        assert_eq!(Some(('h', attr)), it.next());
+        assert_eq!(Some(('i', attr)), it.next());
+        assert_eq!(None, it.next());
+    }
+
+    #[test]
+    fn test_normal_string() {
+        let input = "ab";
+        let ansistring = ANSIParser::default().parse_ansi(input);
+
+        assert_eq!(false, ansistring.has_attrs());
+
+        let mut it = ansistring.iter();
+        assert_eq!(Some(('a', Attr::default())), it.next());
+        assert_eq!(Some(('b', Attr::default())), it.next());
+        assert_eq!(None, it.next());
+
+        assert_eq!("ab", ansistring.into_inner())
     }
 }
+
