99415486efc464bc5ffcebf6fd02a86e8ef695e4
Jef <jackefransham@gmail.com>
2019-08-28 10:02:15 +0200

    Use `to_string` instead of format

--- src/query.rs
+++ src/query.rs
@@ -126,11 +126,9 @@ impl Query {
         let out = if arg.is_empty() {
             self.base_cmd.replace(&self.replstr[..], "")
         } else {
-            format!(
-                "{}",
-                self.base_cmd
-                    .lazy_replace(&self.replstr[..], format_args!("'{}'", escape_single_quote(&arg)))
-            )
+            self.base_cmd
+                .lazy_replace(&self.replstr[..], format_args!("'{}'", escape_single_quote(&arg)))
+                .to_string()
         };
         out
     }
