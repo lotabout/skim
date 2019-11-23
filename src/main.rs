413680e0de9df72d9b1b604e11a74db5df259806
Jinzhou Zhang <lotabout@gmail.com>
2019-11-23 17:13:42 +0800

    Fix #214: USAGE doesn't match code for cmd option

--- src/main.rs
+++ src/main.rs
@@ -160,7 +160,7 @@ fn real_main() -> i32 {
         .arg(Arg::with_name("tiebreak").long("tiebreak").short("t").multiple(true).takes_value(true))
         .arg(Arg::with_name("ansi").long("ansi").multiple(true))
         .arg(Arg::with_name("exact").long("exact").short("e").multiple(true))
-        .arg(Arg::with_name("cmd").long("cmd").short("cmd").multiple(true).takes_value(true))
+        .arg(Arg::with_name("cmd").long("cmd").short("c").multiple(true).takes_value(true))
         .arg(Arg::with_name("interactive").long("interactive").short("i").multiple(true))
         .arg(Arg::with_name("query").long("query").short("q").multiple(true).takes_value(true))
         .arg(Arg::with_name("cmd-query").long("cmd-query").multiple(true).takes_value(true))
