lotabout/skim

commit 1d72fcac572f17039900905e232981f1ad8e07ef

remove the usage of ItemWrapper to reduce memory

src/item.rs
@@ -12,7 +12,7 @@ use regex::Regex;
 use crate::ansi::{ANSIParser, AnsiString};
 use crate::field::{parse_matching_fields, parse_transform_fields, FieldRange};
 use crate::spinlock::{SpinLock, SpinLockGuard};
-use crate::{ItemPreview, SkimItem};
+use crate::SkimItem;
 
 //------------------------------------------------------------------------------
 /// An item will store everything that one line input will need to be operated and displayed.
@@ -123,53 +123,6 @@ impl SkimItem for DefaultSkimItem {
 //------------------------------------------------------------------------------
 pub type ItemIndex = (u32, u32);
 
-pub struct ItemWrapper {
-    inner: Arc<dyn SkimItem>,
-    // (num of run, number of index)
-    id: ItemIndex,
-}
-
-impl ItemWrapper {
-    pub fn new(item: Arc<dyn SkimItem>, index: ItemIndex) -> Self {
-        Self { id: index, inner: item }
-    }
-
-    pub fn get_id(&self) -> ItemIndex {
-        self.id
-    }
-
-    pub fn get_index(&self) -> usize {
-        self.id.1 as usize
-    }
-
-    pub fn get_inner(&self) -> Arc<dyn SkimItem> {
-        self.inner.clone()
-    }
-}
-
-/// delegate to inner
-impl SkimItem for ItemWrapper {
-    fn display(&self) -> Cow<AnsiString> {
-        self.inner.display()
-    }
-
-    fn text(&self) -> Cow<str> {
-        self.inner.text()
-    }
-
-    fn preview(&self) -> ItemPreview {
-        self.inner.preview()
-    }
-
-    fn output(&self) -> Cow<str> {
-        self.inner.output()
-    }
-
-    fn get_matching_ranges(&self) -> Cow<[(usize, usize)]> {
-        self.inner.get_matching_ranges()
-    }
-}
-
 //------------------------------------------------------------------------------
 #[derive(Debug, Copy, Clone, PartialEq, Default)]
 pub struct Rank {
@@ -188,13 +141,13 @@ pub enum MatchedRange {
 
 #[derive(Clone)]
 pub struct MatchedItem {
-    pub item: Arc<ItemWrapper>,
+    pub item: Arc<dyn SkimItem>,
     pub rank: Rank,
     pub matched_range: Option<MatchedRange>, // range of chars that matched the pattern
 }
 
 impl MatchedItem {
-    pub fn builder(item: Arc<ItemWrapper>) -> Self {
+    pub fn builder(item: Arc<dyn SkimItem>) -> Self {
         MatchedItem {
             item,
             rank: Rank::default(),
@@ -233,12 +186,12 @@ const ITEM_POOL_CAPACITY: usize = 1024;
 
 pub struct ItemPool {
     length: AtomicUsize,
-    pool: SpinLock<Vec<Arc<ItemWrapper>>>,
+    pool: SpinLock<Vec<Arc<dyn SkimItem>>>,
     /// number of items that was `take`n
     taken: AtomicUsize,
 
     /// reverse first N lines as header
-    reserved_items: SpinLock<Vec<Arc<ItemWrapper>>>,
+    reserved_items: SpinLock<Vec<Arc<dyn SkimItem>>>,
     lines_to_reserve: usize,
 }
 
@@ -281,7 +234,7 @@ impl ItemPool {
         self.taken.store(0, Ordering::SeqCst);
     }
 
-    pub fn append(&self, mut items: Vec<Arc<ItemWrapper>>) {
+    pub fn append(&self, mut items: Vec<Arc<dyn SkimItem>>) {
         let mut pool = self.pool.lock();
         let mut header_items = self.reserved_items.lock();
 
@@ -296,13 +249,13 @@ impl ItemPool {
         self.length.store(pool.len(), Ordering::SeqCst);
     }
 
-    pub fn take(&self) -> ItemPoolGuard<Arc<ItemWrapper>> {
+    pub fn take(&self) -> ItemPoolGuard<Arc<dyn SkimItem>> {
         let guard = self.pool.lock();
         let taken = self.taken.swap(guard.len(), Ordering::SeqCst);
         ItemPoolGuard { guard, start: taken }
     }
 
-    pub fn reserved(&self) -> ItemPoolGuard<Arc<ItemWrapper>> {
+    pub fn reserved(&self) -> ItemPoolGuard<Arc<dyn SkimItem>> {
         let guard = self.reserved_items.lock();
         ItemPoolGuard { guard, start: 0 }
     }

src/model.rs
@@ -15,7 +15,7 @@ use crate::engine::factory::{AndOrEngineFactory, ExactOrFuzzyEngineFactory, Rege
 use crate::event::{Event, EventHandler, EventReceiver, EventSender};
 use crate::header::Header;
 use crate::input::parse_action_arg;
-use crate::item::{ItemPool, ItemWrapper};
+use crate::item::ItemPool;
 use crate::matcher::{Matcher, MatcherControl};
 use crate::options::SkimOptions;
 use crate::output::SkimOutput;
@@ -352,27 +352,27 @@ impl Model {
     }
 
     fn act_execute_silent(&mut self, cmd: &str) {
-        let item = self.selection.get_current_item();
-        if depends_on_items(cmd) && item.is_none() {
+        let current_index = self.selection.get_current_item_idx();
+        let current_item = self.selection.get_current_item();
+        if depends_on_items(cmd) && current_item.is_none() {
             debug!("act_execute_silent: command refers to items and there is no item for now");
             debug!("command to execute: [{}]", cmd);
             return;
         }
 
-        let current_selection = item
+        let current_selection = current_item
             .as_ref()
             .map(|item| item.output())
             .unwrap_or_else(|| Cow::Borrowed(""));
         let query = self.query.get_fz_query();
         let cmd_query = self.query.get_cmd_query();
 
-        let selections = self.selection.get_selected_wrapped_items();
+        let (indices, selections) = self.selection.get_selected_indices_and_items();
         let tmp: Vec<Cow<str>> = selections.iter().map(|item| item.text()).collect();
         let selected_texts: Vec<&str> = tmp.iter().map(|cow| cow.as_ref()).collect();
-        let indices: Vec<usize> = selections.iter().map(|x| x.get_index()).collect();
 
         let context = InjectContext {
-            current_index: item.as_ref().map(|x| x.get_index()).unwrap_or(0),
+            current_index,
             delimiter: &self.delimiter,
             current_selection: &current_selection,
             selections: &selected_texts,
@@ -395,10 +395,7 @@ impl Model {
 
         let item_index = (std::u32::MAX, self.next_idx_to_append);
 
-        let item: Arc<ItemWrapper> = Arc::new(ItemWrapper::new(
-            Arc::new(query),
-            (std::u32::MAX, self.next_idx_to_append),
-        ));
+        let item: Arc<dyn SkimItem> = Arc::new(query);
 
         self.next_idx_to_append += 1;
 
@@ -475,7 +472,7 @@ impl Model {
                         accept_key,
                         query: self.query.get_fz_query(),
                         cmd: self.query.get_cmd_query(),
-                        selected_items: self.selection.get_selected_items(),
+                        selected_items: self.selection.get_selected_indices_and_items().1,
                     });
                 }
 
@@ -555,11 +552,13 @@ impl Model {
 
             // re-draw
             if !self.preview_hidden {
+                let item_index = self.selection.get_current_item_idx();
                 let item = self.selection.get_current_item();
                 if let Some(previewer) = self.previewer.as_mut() {
                     let selections = &self.selection;
-                    let get_selected_items = || selections.get_selected_wrapped_items();
+                    let get_selected_items = || selections.get_selected_indices_and_items();
                     previewer.on_item_change(
+                        item_index,
                         item,
                         env.query.to_string(),
                         env.cmd_query.to_string(),

src/selection.rs
@@ -11,7 +11,7 @@ use tuikit::prelude::{Event as TermEvent, *};
 use crate::event::{Event, EventHandler, UpdateScreen};
 use crate::global::current_run_num;
 use crate::item::{parse_criteria, ItemIndex, RankCriteria};
-use crate::item::{ItemWrapper, MatchedItem, MatchedRange};
+use crate::item::{MatchedItem, MatchedRange};
 use crate::orderedvec::CompareFunction;
 use crate::orderedvec::OrderedVec;
 use crate::spinlock::SpinLock;
@@ -29,7 +29,7 @@ lazy_static! {
 pub struct Selection {
     // all items
     items: OrderedVec<MatchedItem>,
-    selected: BTreeMap<ItemIndex, Arc<ItemWrapper>>,
+    selected: BTreeMap<ItemIndex, Arc<dyn SkimItem>>,
 
     //
     // |>------ items[items.len()-1]
@@ -219,7 +219,7 @@ impl Selection {
         }
     }
 
-    pub fn act_select_item(&mut self, item_index: ItemIndex, item: Arc<ItemWrapper>) {
+    pub fn act_select_item(&mut self, item_index: ItemIndex, item: Arc<dyn SkimItem>) {
         if !self.multi_selection {
             return;
         }
@@ -250,10 +250,11 @@ impl Selection {
         self.hscroll_offset = hscroll_offset as usize;
     }
 
-    pub fn get_selected_wrapped_items(&self) -> Vec<Arc<ItemWrapper>> {
+    pub fn get_selected_indices_and_items(&self) -> (Vec<usize>, Vec<Arc<dyn SkimItem>>) {
         // select the current one
         let select_cursor = !self.multi_selection || self.selected.is_empty();
-        let mut selected: Vec<Arc<ItemWrapper>> = self.selected.values().cloned().collect();
+        let mut selected: Vec<Arc<dyn SkimItem>> = self.selected.values().cloned().collect();
+        let mut item_indices: Vec<usize> = self.selected.keys().map(|(_run, idx)| *idx as usize).collect();
 
         if select_cursor && !self.items.is_empty() {
             let cursor = self.item_cursor + self.line_cursor;
@@ -262,18 +263,11 @@ impl Selection {
                 .get(cursor)
                 .unwrap_or_else(|| panic!("model:act_output: failed to get item {}", cursor));
             let item = current_item.item.clone();
+            item_indices.push(cursor);
             selected.push(item);
         }
 
-        selected.sort_by_key(|item| item.get_id());
-        selected
-    }
-
-    pub fn get_selected_items(&self) -> Vec<Arc<dyn SkimItem>> {
-        self.get_selected_wrapped_items()
-            .into_iter()
-            .map(|wrapped| wrapped.get_inner())
-            .collect()
+        (item_indices, selected)
     }
 
     pub fn get_num_of_selected_exclude_current(&self) -> usize {
@@ -292,7 +286,7 @@ impl Selection {
         self.multi_selection
     }
 
-    pub fn get_current_item(&self) -> Option<Arc<ItemWrapper>> {
+    pub fn get_current_item(&self) -> Option<Arc<dyn SkimItem>> {
         let item_idx = self.get_current_item_idx();
         self.items.get(item_idx).map(|item| item.item.clone())
     }

src/bin/main.rs
@@ -10,7 +10,6 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::os::unix::io::AsRawFd;
-use std::sync::atomic::AtomicU32;
 
 use clap::{App, Arg, ArgMatches};
 use nix::unistd::isatty;
@@ -454,7 +453,6 @@ pub fn filter(options: &SkimOptions, source: Option<SkimItemReceiver>) -> Result
 
     //------------------------------------------------------------------------------
     // start
-    let item_index = AtomicU32::new(0);
     let components_to_stop = Arc::new(AtomicUsize::new(0));
     let collector_option = CollectorOption::with_options(&options);
 
@@ -467,8 +465,7 @@ pub fn filter(options: &SkimOptions, source: Option<SkimItemReceiver>) -> Result
     let mut num_matched = 0;
     stream_of_item
         .into_iter()
-        .map(|item| ItemWrapper::new(item, (0, item_index.fetch_add(0, Ordering::SeqCst))))
-        .filter_map(|wrapped| engine.match_item(Arc::new(wrapped)))
+        .filter_map(|item| engine.match_item(item))
         .try_for_each(|matched| {
             num_matched += 1;
             if options.print_score {

src/engine/all.rs
@@ -1,8 +1,8 @@
 use std::fmt::{Display, Error, Formatter};
 use std::sync::Arc;
 
-use crate::item::{ItemWrapper, MatchedItem, MatchedRange, Rank};
-use crate::MatchEngine;
+use crate::item::{MatchedItem, MatchedRange, Rank};
+use crate::{MatchEngine, SkimItem};
 
 //------------------------------------------------------------------------------
 #[derive(Debug)]
@@ -19,7 +19,7 @@ impl MatchAllEngine {
 }
 
 impl MatchEngine for MatchAllEngine {
-    fn match_item(&self, item: Arc<ItemWrapper>) -> Option<MatchedItem> {
+    fn match_item(&self, item: Arc<dyn SkimItem>) -> Option<MatchedItem> {
         let rank = Rank {
             score: 0,
             begin: 0,

src/engine/andor.rs
@@ -1,8 +1,8 @@
 use std::fmt::{Display, Error, Formatter};
 use std::sync::Arc;
 
-use crate::item::{ItemWrapper, MatchedItem, MatchedRange};
-use crate::MatchEngine;
+use crate::item::{MatchedItem, MatchedRange};
+use crate::{MatchEngine, SkimItem};
 
 //------------------------------------------------------------------------------
 // OrEngine, a combinator
@@ -26,7 +26,7 @@ impl OrEngine {
 }
 
 impl MatchEngine for OrEngine {
-    fn match_item(&self, item: Arc<ItemWrapper>) -> Option<MatchedItem> {
+    fn match_item(&self, item: Arc<dyn SkimItem>) -> Option<MatchedItem> {
         for engine in &self.engines {
             let result = engine.match_item(Arc::clone(&item));
             if result.is_some() {
@@ -98,7 +98,7 @@ impl AndEngine {
 }
 
 impl MatchEngine for AndEngine {
-    fn match_item(&self, item: Arc<ItemWrapper>) -> Option<MatchedItem> {
+    fn match_item(&self, item: Arc<dyn SkimItem>) -> Option<MatchedItem> {
         // mock
         let mut results = vec![];
         for engine in &self.engines {

src/engine/exact.rs
@@ -1,7 +1,6 @@
 use crate::engine::util::{contains_upper, regex_match};
-use crate::item::{ItemWrapper, MatchedItem, MatchedRange, Rank};
-use crate::SkimItem;
-use crate::{CaseMatching, MatchEngine};
+use crate::item::{MatchedItem, MatchedRange, Rank};
+use crate::{CaseMatching, MatchEngine, SkimItem};
 use regex::{escape, Regex};
 use std::fmt::{Display, Error, Formatter};
 use std::sync::Arc;
@@ -66,7 +65,7 @@ impl ExactEngine {
 }
 
 impl MatchEngine for ExactEngine {
-    fn match_item(&self, item: Arc<ItemWrapper>) -> Option<MatchedItem> {
+    fn match_item(&self, item: Arc<dyn SkimItem>) -> Option<MatchedItem> {
         let mut matched_result = None;
         for &(start, end) in item.get_matching_ranges().as_ref() {
             if self.query_regex.is_none() {

src/engine/fuzzy.rs
@@ -5,7 +5,7 @@ use fuzzy_matcher::clangd::ClangdMatcher;
 use fuzzy_matcher::skim::{SkimMatcher, SkimMatcherV2};
 use fuzzy_matcher::FuzzyMatcher;
 
-use crate::item::{ItemWrapper, MatchedItem, MatchedRange, Rank};
+use crate::item::{MatchedItem, MatchedRange, Rank};
 use crate::SkimItem;
 use crate::{CaseMatching, MatchEngine};
 
@@ -113,7 +113,7 @@ impl FuzzyEngine {
 }
 
 impl MatchEngine for FuzzyEngine {
-    fn match_item(&self, item: Arc<ItemWrapper>) -> Option<MatchedItem> {
+    fn match_item(&self, item: Arc<dyn SkimItem>) -> Option<MatchedItem> {
         // iterate over all matching fields:
         let mut matched_result = None;
         for &(start, end) in item.get_matching_ranges().as_ref() {

commit d0f46b671e6c021383d020f8ed609b30ba053102

fix: multiple selection (regression in 1d72fca)

src/item.rs
@@ -10,9 +10,6 @@ use crate::spinlock::{SpinLock, SpinLockGuard};
 use crate::{MatchRange, Rank, SkimItem};
 
 //------------------------------------------------------------------------------
-pub type ItemIndex = (u32, u32);
-
-//------------------------------------------------------------------------------
 
 #[derive(Debug)]
 pub struct RankBuilder {
@@ -130,6 +127,10 @@ impl ItemPool {
         self.length.load(Ordering::SeqCst) - self.taken.load(Ordering::SeqCst)
     }
 
+    pub fn num_taken(&self) -> usize {
+        self.taken.load(Ordering::SeqCst)
+    }
+
     pub fn clear(&self) {
         let mut items = self.pool.lock();
         items.clear();
@@ -145,7 +146,8 @@ impl ItemPool {
         self.taken.store(0, Ordering::SeqCst);
     }
 
-    pub fn append(&self, mut items: Vec<Arc<dyn SkimItem>>) {
+    /// append the items and return the new_size of the pool
+    pub fn append(&self, mut items: Vec<Arc<dyn SkimItem>>) -> usize {
         let len = items.len();
         trace!("item pool, append {} items", len);
         let mut pool = self.pool.lock();
@@ -161,6 +163,7 @@ impl ItemPool {
         }
         self.length.store(pool.len(), Ordering::SeqCst);
         trace!("item pool, done append {} items", len);
+        pool.len()
     }
 
     pub fn take(&self) -> ItemPoolGuard<Arc<dyn SkimItem>> {

src/matcher.rs
@@ -83,7 +83,7 @@ impl Matcher {
         let matched_items_clone = matched_items.clone();
 
         let thread_matcher = thread::spawn(move || {
-            let num_taken = item_pool.num_not_taken();
+            let num_taken = item_pool.num_taken();
             let items = item_pool.take();
 
             // 1. use rayon for parallel

src/model.rs
@@ -14,9 +14,10 @@ use tuikit::prelude::{Event as TermEvent, *};
 
 use crate::engine::factory::{AndOrEngineFactory, ExactOrFuzzyEngineFactory, RegexEngineFactory};
 use crate::event::{Event, EventHandler, EventReceiver, EventSender};
+use crate::global::current_run_num;
 use crate::header::Header;
 use crate::input::parse_action_arg;
-use crate::item::{parse_criteria, ItemPool, RankBuilder, RankCriteria};
+use crate::item::{parse_criteria, ItemPool, MatchedItem, RankBuilder, RankCriteria};
 use crate::matcher::{Matcher, MatcherControl};
 use crate::options::SkimOptions;
 use crate::output::SkimOutput;
@@ -27,7 +28,8 @@ use crate::selection::Selection;
 use crate::spinlock::SpinLock;
 use crate::theme::ColorTheme;
 use crate::util::{depends_on_items, inject_command, margin_string_to_size, parse_margin, InjectContext};
-use crate::{FuzzyAlgorithm, MatchEngineFactory, SkimItem};
+use crate::{FuzzyAlgorithm, MatchEngineFactory, MatchRange, SkimItem};
+use std::cmp::max;
 
 const REFRESH_DURATION: i64 = 100;
 const SPINNER_DURATION: u32 = 200;
@@ -89,7 +91,8 @@ pub struct Model {
     timer: Timer,
     hb_timer_guard: Option<TimerGuard>,
 
-    next_idx_to_append: u32, // for AppendAndSelect action
+    // for AppendAndSelect action
+    rank_builder: Arc<RankBuilder>,
 }
 
 impl Model {
@@ -125,7 +128,7 @@ impl Model {
             let fuzzy_engine_factory: Rc<dyn MatchEngineFactory> = Rc::new(AndOrEngineFactory::new(
                 ExactOrFuzzyEngineFactory::builder()
                     .exact_mode(options.exact)
-                    .rank_builder(rank_builder)
+                    .rank_builder(rank_builder.clone())
                     .build(),
             ));
             Matcher::builder(fuzzy_engine_factory).case(options.case).build()
@@ -183,7 +186,7 @@ impl Model {
             timer: Timer::new(),
             hb_timer_guard: None,
 
-            next_idx_to_append: 0,
+            rank_builder,
         };
         ret.parse_options(options);
         ret
@@ -443,14 +446,18 @@ impl Model {
             return;
         }
 
-        let item_index = (std::u32::MAX, self.next_idx_to_append);
-
+        let item_len = query.len();
         let item: Arc<dyn SkimItem> = Arc::new(query);
+        let new_len = self.item_pool.append(vec![item.clone()]);
+        let item_idx = (max(new_len, 1) - 1) as u32;
+        let matched_item = MatchedItem {
+            item,
+            rank: self.rank_builder.build_rank(0, 0, 0, item_len),
+            matched_range: Some(MatchRange::ByteRange(0, 0)),
+            item_idx,
+        };
 
-        self.next_idx_to_append += 1;
-
-        self.item_pool.append(vec![item.clone()]);
-        self.selection.act_select_item(item_index, item);
+        self.selection.act_select_item(current_run_num(), matched_item);
 
         self.act_heart_beat(env);
     }
@@ -654,7 +661,7 @@ impl Model {
         if !processed {
             // take out new items and put them into items
             let new_items = self.reader_control.as_ref().map(|c| c.take()).unwrap();
-            self.item_pool.append(new_items);
+            let _ = self.item_pool.append(new_items);
         };
 
         // send heart beat (so that heartbeat/refresh is triggered)

src/selection.rs
@@ -9,7 +9,6 @@ use tuikit::prelude::{Event as TermEvent, *};
 ///! Handle the selections of items
 use crate::event::{Event, EventHandler, UpdateScreen};
 use crate::global::current_run_num;
-use crate::item::ItemIndex;
 use crate::item::MatchedItem;
 use crate::orderedvec::OrderedVec;
 use crate::theme::{ColorTheme, DEFAULT_THEME};
@@ -17,6 +16,8 @@ use crate::util::{print_item, reshape_string, LinePrinter};
 use crate::{DisplayContext, MatchRange, Matches, SkimItem, SkimOptions};
 use regex::Regex;
 use unicode_width::UnicodeWidthStr;
+
+type ItemIndex = (u32, u32);
 
 pub struct Selection {
     // all items
@@ -183,7 +184,7 @@ impl Selection {
             .items
             .get(cursor)
             .unwrap_or_else(|| panic!("model:act_toggle: failed to get item {}", cursor));
-        let index = (current_run_num(), cursor as u32);
+        let index = (current_run_num(), current_item.item_idx);
         if !self.selected.contains_key(&index) {
             self.selected.insert(index, current_item.item.clone());
         } else {
@@ -198,8 +199,8 @@ impl Selection {
         }
 
         let run_num = current_run_num();
-        for (idx, current_item) in self.items.iter().enumerate() {
-            let index = (run_num, idx as u32);
+        for current_item in self.items.iter() {
+            let index = (run_num, current_item.item_idx);
             if !self.selected.contains_key(&index) {
                 self.selected.insert(index, current_item.item.clone());
             } else {
@@ -208,12 +209,12 @@ impl Selection {
         }
     }
 
-    pub fn act_select_item(&mut self, item_index: ItemIndex, item: Arc<dyn SkimItem>) {
+    pub fn act_select_item(&mut self, run_num: u32, matched: MatchedItem) {
         if !self.multi_selection {
             return;
         }
 
-        self.selected.insert(item_index, item);
+        self.selected.insert((run_num, matched.item_idx), matched.item.clone());
     }
 
     pub fn act_select_all(&mut self) {
@@ -222,9 +223,9 @@ impl Selection {
         }
 
         let run_num = current_run_num();
-        for (idx, current_item) in self.items.iter().enumerate() {
+        for current_item in self.items.iter() {
             let item = current_item.item.clone();
-            self.selected.insert((run_num, idx as u32), item);
+            self.selected.insert((run_num, current_item.item_idx), item);
         }
     }
 
@@ -359,7 +360,6 @@ impl Selection {
         canvas: &mut dyn Canvas,
         row: usize,
         matched_item: &MatchedItem,
-        item_index: usize,
         is_current: bool,
     ) -> Result<()> {
         let (screen_width, screen_height) = canvas.size()?;
@@ -384,7 +384,7 @@ impl Selection {
         };
 
         // print selection cursor
-        let index = (current_run_num(), item_index as u32);
+        let index = (current_run_num(), matched_item.item_idx);
         if self.selected.contains_key(&index) {
             let _ = canvas.print_with_attr(row, 1, ">", default_attr.extend(self.theme.selected()));
         } else {
@@ -505,7 +505,7 @@ impl Draw for Selection {
                 .get(item_idx)
                 .unwrap_or_else(|| panic!("model:draw_items: failed to get item at {}", item_idx));
 
-            let _ = self.draw_item(canvas, line_no, &item, item_idx, line_cursor == self.line_cursor);
+            let _ = self.draw_item(canvas, line_no, &item, line_cursor == self.line_cursor);
         }
 
         Ok(())

test/test_skim.py
@@ -965,6 +965,40 @@ class TestSkim(TestBase):
         self.tmux.until(lambda lines: lines.ready_with_lines(1))
         self.tmux.until(lambda lines: lines.any_include('..c'))
 
+    def test_multi_selection(self):
+        self.tmux.send_keys(f"""echo -n 'a\nb\nc' | {self.sk("-m")}""", Key('Enter'))
+        self.tmux.until(lambda lines: lines.ready_with_lines(3))
+        self.tmux.until(lambda lines: lines[-3].startswith('> a'))
+        self.tmux.send_keys(Key('b'))
+        self.tmux.until(lambda lines: lines[-3].startswith('> b'))
+        self.tmux.send_keys(Key('TAB'))
+        self.tmux.until(lambda lines: lines[-3].startswith('>>b'))
+        self.tmux.send_keys(Key('C-h'))
+        self.tmux.until(lambda lines: lines[-4].startswith(' >b'))
+        self.tmux.until(lambda lines: lines[-3].startswith('> a'))
+        self.tmux.send_keys(Key('c'))
+        self.tmux.until(lambda lines: lines[-3].startswith('> c'))
+        self.tmux.send_keys(Key('TAB'))
+        self.tmux.until(lambda lines: lines[-3].startswith('>>c'))
+        self.tmux.send_keys(Key('C-h'))
+        self.tmux.until(lambda lines: lines[-5].startswith(' >c'))
+        self.tmux.until(lambda lines: lines[-4].startswith(' >b'))
+        self.tmux.until(lambda lines: lines[-3].startswith('> a'))
+        self.tmux.send_keys(Key('Enter'))
+        self.assertEqual('b\nc', self.readonce().strip())
+
+    def test_append_and_select(self):
+        self.tmux.send_keys(f"""echo -n 'a\nb\nc' | {self.sk("-m --bind 'ctrl-f:append-and-select'")}""", Key('Enter'))
+        self.tmux.until(lambda lines: lines.ready_with_lines(3))
+        self.tmux.send_keys(Key('xyz'))
+        self.tmux.until(lambda lines: lines.ready_with_matches(0))
+        self.tmux.send_keys(Key('C-f'))
+        self.tmux.until(lambda lines: lines[-3].startswith('>>xyz'))
+        self.tmux.send_keys(Key('C-u'))
+        self.tmux.until(lambda lines: lines[-6].startswith(' >xyz'))
+        self.tmux.until(lambda lines: lines[-5].startswith('  c'))
+        self.tmux.until(lambda lines: lines[-4].startswith('  b'))
+        self.tmux.until(lambda lines: lines[-3].startswith('> a'))
 
 def find_prompt(lines, interactive=False, reverse=False):
     linen = -1